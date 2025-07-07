0e25a196c9100f6154dc471d21babefcf03d4add
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2025-07-07 13:21:51 -0400

    Refactor build script and debug build support

--- scripts/copy.ps1
+++ scripts/copy.ps1
@@ -1,11 +1,23 @@
-& $PSScriptRoot/build.ps1
+param(
+    [Parameter(ValueFromRemainingArguments=$true)]
+    [string[]]$args,
+    [switch]$log = $false
+)
+
+# Forward the "release" argument to build.ps1 if it exists
+$buildArgs = @()
+if ($args -contains "release") {
+    $buildArgs += "release"
+}
+
+& $PSScriptRoot/build.ps1 @buildArgs
 if ($?) {
     adb push build/libtracks.so /sdcard/ModData/com.beatgames.beatsaber/Modloader/mods/libtracks.so
     if ($?) {
         adb shell am force-stop com.beatgames.beatsaber
         adb shell am start com.beatgames.beatsaber/com.unity3d.player.UnityPlayerActivity
 #         adb logcat -c; adb logcat > test.log
-        if ($args[0] -eq "--log") {
+        if ($log.IsPresent) {
             $timestamp = Get-Date -Format "MM-dd HH:mm:ss.fff"
 #             adb logcat -c
 #             adb logcat -T "$timestamp" main-modloader:W QuestHook[Tracks`|v0.1.0]:* QuestHook[UtilsLogger`|v1.0.12]:* AndroidRuntime:E *:S
