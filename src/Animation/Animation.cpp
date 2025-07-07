0b73e6834a779da0f1c2cad18ed6d0e4f4d53c9b
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2025-07-07 15:37:07 -0400

    Fix use after free for named point definitions

--- src/Animation/Animation.cpp
+++ src/Animation/Animation.cpp
@@ -10,6 +10,7 @@ namespace Animation {
 PointDefinitionW TryGetPointData(BeatmapAssociatedData& beatmapAD, rapidjson::Value const& customData,
                                  std::string_view pointName, Tracks::ffi::WrapBaseValueType type) {
   PointDefinitionW pointData = PointDefinitionW(nullptr);
+  auto tracksContext = beatmapAD.internal_tracks_context;
 
   auto customDataItr = customData.FindMember(pointName.data());
   if (customDataItr == customData.MemberEnd()) {
@@ -21,27 +22,31 @@ PointDefinitionW TryGetPointData(BeatmapAssociatedData& beatmapAD, rapidjson::Va
   case rapidjson::kNullType:
     return pointData;
   case rapidjson::kStringType: {
-
     auto id = pointString.GetString();
+    auto existing = tracksContext->GetPointDefinition(id, type);
+    if (existing) {
+      TLogger::Logger.fmtLog<Paper::LogLevel::INF>("Using existing point definition {} {}", id, (int)type);
+      return existing.value();
+    }
+
     auto itr = beatmapAD.pointDefinitionsRaw.find(id);
     if (itr == beatmapAD.pointDefinitionsRaw.end()) {
       TLogger::Logger.warn("Could not find point definition {}", pointString.GetString());
-    } else {
-      auto json = convert_rapidjson(*itr->second);
-      auto tracksContext = beatmapAD.internal_tracks_context;
-      auto baseProviderContext = tracksContext->GetBaseProviderContext();
-      auto pointDataAnon = Tracks::ffi::tracks_make_base_point_definition(json, type, baseProviderContext);
-      pointData = tracksContext->AddPointDefinition(id, pointDataAnon);
+      return pointData;
     }
+    TLogger::Logger.fmtLog<Paper::LogLevel::INF>("Using point definition {} {}", pointString.GetString(), (int)type);
+    auto json = convert_rapidjson(*itr->second);
+    auto baseProviderContext = tracksContext->GetBaseProviderContext();
+    auto pointDataAnon = Tracks::ffi::tracks_make_base_point_definition(json, type, baseProviderContext);
+    pointData = tracksContext->AddPointDefinition(id, pointDataAnon);
 
     break;
   }
   default:
     auto json = convert_rapidjson(pointString);
-    auto tracksContext = beatmapAD.internal_tracks_context;
     auto baseProviderContext = tracksContext->GetBaseProviderContext();
     auto pointDataAnon = Tracks::ffi::tracks_make_base_point_definition(json, type, baseProviderContext);
-    pointData = tracksContext->AddPointDefinition("", pointDataAnon);
+    pointData = tracksContext->AddPointDefinition(std::nullopt, pointDataAnon);
   }
 
   return pointData;
